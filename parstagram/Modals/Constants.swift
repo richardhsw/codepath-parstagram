c378c4bce3d92eb08b544095d590d729a693fcf2
Richard Hsu <shengwh1@uci.edu>
2020-02-15 15:32:48 -0800

    connect to parse server

---
+++ parstagram/Modals/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  parstagram
+//
+//  Created by Richard Hsu on 2020/2/15.
+//  Copyright © 2020 Richard Hsu. All rights reserved.
+//
+
+import Foundation
+
+enum HerokuServer : String{
+    case appID     = "parstagram"
+    case serverURL = "https://rocky-shore-36635.herokuapp.com/parse"
+}
