9d59ac67f3d0381981151ea4000a6832adaf4b57
Richard Hsu <shengwh1@uci.edu>
2020-02-15 17:59:10 -0800

    sign up + login

--- parstagram/Modals/Constants.swift
+++ parstagram/Modals/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
-enum HerokuServer : String{
+enum HerokuServer : String {
     case appID     = "parstagram"
     case serverURL = "https://rocky-shore-36635.herokuapp.com/parse"
+}
+
+enum Segues : String {
+    case login = "loginSegue"
+}
+
+enum Errors : String {
+    case generalTitle = "Error"
+    case emptyUPMessage = "Username and password field cannot be empty"
+    case loginTitle = "Login Error"
+    case loginMessage = "Oops! Something went wrong while logging in:\n"
+    case signupTitle = "Sign up error"
+    case signupMessage = "Oops! Something went wrong while signing up:\n"
 }
--- parstagram/ViewControllers/LoginViewController.swift
+++ parstagram/ViewControllers/LoginViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
     
     // MARK: - Variables
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
     
@@ -22,20 +25,90 @@ class LoginViewController: UIViewController {
     }
     
     
-    // MARK: - Helper Functions
+    // MARK: - UI Customization Functions
     func customizeButton() {
         loginButton.layer.cornerRadius = 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Helper Functions
+    /*------ Handle text field inputs  ------*/
+    func usernameAndPasswordNotEmpty() -> Bool {
+        // Check text field inputs
+        if usernameField.text!.isEmpty || passwordField.text!.isEmpty {
+            displayError()
+            return false
+        } else {
+            return true
+        }
     }
-    */
+    
+    
+    // MARK: - Alert Controlers
+    // Text fields are empty alert controller
+    func displayError() {
+        let alertController = UIAlertController(title: Errors.generalTitle.rawValue, message: Errors.emptyUPMessage.rawValue, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    // Login error alert controller
+    func displayLoginError(error: Error) {
+        let message = Errors.loginMessage.rawValue + "\(error.localizedDescription)"
+        let alertController = UIAlertController(title: Errors.loginTitle.rawValue, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    // Sign up error alert controller
+    func displaySignupError(error: Error) {
+        let message = Errors.signupMessage.rawValue + "\(error.localizedDescription)"
+        let alertController = UIAlertController(title: Errors.signupTitle.rawValue, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    
+    // MARK: - Action Functions
+    @IBAction func onLogin(_ sender: Any) {
+        if usernameAndPasswordNotEmpty() {
+            let username = usernameField.text!
+            let password = passwordField.text!
+            
+            PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+                if user != nil {
+                    self.performSegue(withIdentifier: Segues.login.rawValue, sender: nil)
+                }
+                else {
+                    self.displayLoginError(error: error!)
+                }
+            }
+        }
+    }
 
+    @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: Segues.login.rawValue, sender: nil)
+            }
+            else {
+                self.displaySignupError(error: error!)
+            }
+        }
+    }
+    
+    
 }
